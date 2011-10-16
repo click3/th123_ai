d20ac98e0f491992f15d7683ba6f43142a20220d
sweetie <bnryxx332a@mail.goo.ne.jp>
2011-10-17 00:44:26 +0900

    scriptBase.cppのインデントを調整

--- th123_ai/scriptBase.cpp
+++ th123_ai/scriptBase.cpp
@@ -6,183 +6,183 @@
 #define SCRIPT_NAME_EMPTY	("memory")
 
 //private
-	void scriptEngine::convertFilePath(char *path) const {
-		if(!loadable) {
-			yield();
-		}
-		while(true) {
-			char ss[256], *s = ::strstr(path,"%");
-			if(s == NULL) {
-				break;
-			}
-			if(strlen(s) < 4) {
-				s[0] = '\0';
-				break;
-			} else if((s[1]!='c' && s[1]!='C') || (s[2]!='1' && s[2]!='2')) {
-				::strmove(s, &s[3]);
-				continue;
-			}
-			const unsigned int char_id = getScriptValue(s[2]=='1' ? "my_char" : "enemy_char");
-			const char * const char_name = (s[1]=='c' ? ::id2char2(char_id) : id2char(char_id));
-			char temp[256];
-			SPRINTF(temp, "%s%s%s", path, char_name, s);
-			::strcpy(path, ss);
-		}
+void scriptEngine::convertFilePath(char *path) const {
+	if(!loadable) {
+		yield();
 	}
+	while(true) {
+		char ss[256], *s = ::strstr(path,"%");
+		if(s == NULL) {
+			break;
+		}
+		if(strlen(s) < 4) {
+			s[0] = '\0';
+			break;
+		} else if((s[1]!='c' && s[1]!='C') || (s[2]!='1' && s[2]!='2')) {
+			::strmove(s, &s[3]);
+			continue;
+		}
+		const unsigned int char_id = getScriptValue(s[2]=='1' ? "my_char" : "enemy_char");
+		const char * const char_name = (s[1]=='c' ? ::id2char2(char_id) : id2char(char_id));
+		char temp[256];
+		SPRINTF(temp, "%s%s%s", path, char_name, s);
+		::strcpy(path, ss);
+	}
+}
 
 
 //public
-	bool scriptEngine::loadFile(const char *fileName){
-		if(fileName==NULL || fileName[0]=='\0' || strlen(fileName)>255){
-			return false;
+bool scriptEngine::loadFile(const char *fileName){
+	if(fileName==NULL || fileName[0]=='\0' || strlen(fileName)>255){
+		return false;
+	}
+	char fullPath[256];
+	strncpy(fullPath, fileName, sizeof(fullPath));
+	convertFilePath(fullPath);
+	char prevDir[256];
+	GetCurrentDirectory(sizeof(prevDir), prevDir);
+	char fn[256];
+	{
+		char dir[256],*s;
+		strncpy(dir, fullPath, sizeof(dir));
+		s = &dir[strlen(dir)];
+		while(s>=dir && s[0]!='\\' && s[0]!='/'){
+			s--;
 		}
-		char fullPath[256];
-		strncpy(fullPath, fileName, sizeof(fullPath));
-		convertFilePath(fullPath);
-		char prevDir[256];
-		GetCurrentDirectory(sizeof(prevDir), prevDir);
-		char fn[256];
-		{
-			char dir[256],*s;
-			strncpy(dir, fullPath, sizeof(dir));
-			s = &dir[strlen(dir)];
-			while(s>=dir && s[0]!='\\' && s[0]!='/'){
-				s--;
-			}
-			if(s >= dir){
-				s[0] = '\0';
-				SetCurrentDirectoryA(dir);
-			}
-			strncpy(fn,&s[1],sizeof(fn));
+		if(s >= dir){
+			s[0] = '\0';
+			SetCurrentDirectoryA(dir);
 		}
-		FILE *fp = fopen(fn, "rb");
-		if(fp == NULL){
-			SetCurrentDirectoryA(prevDir);
-			return false;
-		}
-		bool ret = loadFile(fp, fullPath);
-		fclose(fp);
-		if(ret){
-			strncpy(scriptName, fullPath, sizeof(scriptName));
-		}
+		strncpy(fn,&s[1],sizeof(fn));
+	}
+	FILE *fp = fopen(fn, "rb");
+	if(fp == NULL){
 		SetCurrentDirectoryA(prevDir);
-		return ret;
+		return false;
 	}
-	bool scriptEngine::loadFile(FILE *fp, const char *name){
-		if(fp == NULL){
-			return false;
-		}
-		int start = ftell(fp);
-		fseek(fp,0,SEEK_END);
-		int size = ftell(fp) - start;
-		fseek(fp,start,SEEK_SET);
-		char *data = (char *)malloc(size);
-		if(data == NULL){
-			return false;
-		}
-		fread(data,1,size,fp);
-		fseek(fp,start,SEEK_SET);
-		bool ret = loadBuffer(data, size, name);
-		free(data);
-		return ret;
+	bool ret = loadFile(fp, fullPath);
+	fclose(fp);
+	if(ret){
+		strncpy(scriptName, fullPath, sizeof(scriptName));
 	}
-	bool scriptEngine::loadFile(FILE *fp){
-		return loadFile(fp, SCRIPT_NAME_EMPTY);
+	SetCurrentDirectoryA(prevDir);
+	return ret;
+}
+bool scriptEngine::loadFile(FILE *fp, const char *name){
+	if(fp == NULL){
+		return false;
 	}
-	bool scriptEngine::loadDialog(){
-		OPENFILENAME ofn;
-		char fileName[256] = "";
-		std::string dir_str;
-		org::click3::Utility::GetAppDir(dir_str);
-		const boost::filesystem::path dir = boost::filesystem::path(dir_str) / "script";
-		memset(&ofn,0,sizeof(OPENFILENAME));
+	int start = ftell(fp);
+	fseek(fp,0,SEEK_END);
+	int size = ftell(fp) - start;
+	fseek(fp,start,SEEK_SET);
+	char *data = (char *)malloc(size);
+	if(data == NULL){
+		return false;
+	}
+	fread(data,1,size,fp);
+	fseek(fp,start,SEEK_SET);
+	bool ret = loadBuffer(data, size, name);
+	free(data);
+	return ret;
+}
+bool scriptEngine::loadFile(FILE *fp){
+	return loadFile(fp, SCRIPT_NAME_EMPTY);
+}
+bool scriptEngine::loadDialog(){
+	OPENFILENAME ofn;
+	char fileName[256] = "";
+	std::string dir_str;
+	org::click3::Utility::GetAppDir(dir_str);
+	const boost::filesystem::path dir = boost::filesystem::path(dir_str) / "script";
+	memset(&ofn,0,sizeof(OPENFILENAME));
 
-		ofn.lStructSize = sizeof(OPENFILENAME);
-		ofn.lpstrFilter = "AIファイル(*.ai)\0*.ai\0All files(*.*)\0*.*\0\0";
-		ofn.lpstrFile = fileName;
-		ofn.nMaxFile = sizeof(fileName);
-		ofn.lpstrInitialDir = dir.string().c_str();
-		ofn.Flags = OFN_FILEMUSTEXIST;
-		ofn.lpstrTitle = "ファイルを開く";
-		ofn.lpstrDefExt = "ai";
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.lpstrFilter = "AIファイル(*.ai)\0*.ai\0All files(*.*)\0*.*\0\0";
+	ofn.lpstrFile = fileName;
+	ofn.nMaxFile = sizeof(fileName);
+	ofn.lpstrInitialDir = dir.string().c_str();
+	ofn.Flags = OFN_FILEMUSTEXIST;
+	ofn.lpstrTitle = "ファイルを開く";
+	ofn.lpstrDefExt = "ai";
 
-		if(!GetOpenFileName(&ofn)){
+	if(!GetOpenFileName(&ofn)){
+		return false;
+	}
+	return loadFile(fileName);
+}
+bool scriptEngine::loadResource(int id){
+	char *buffer;
+	int size;
+	{
+		HMODULE hm = GetModuleHandle(NULL);
+		char name[64];
+		snprintf(name, sizeof(name), "#%d",id);
+		HRSRC hr = FindResource(hm, name, "RT_RCDATA");
+		size = SizeofResource(hm, hr);
+		buffer = static_cast<char*>(LockResource(LoadResource(hm, hr)));
+		if(hm==NULL || hr==NULL || size==0 || buffer==NULL){
 			return false;
 		}
-		return loadFile(fileName);
 	}
-	bool scriptEngine::loadResource(int id){
-		char *buffer;
-		int size;
-		{
-			HMODULE hm = GetModuleHandle(NULL);
-			char name[64];
-			snprintf(name, sizeof(name), "#%d",id);
-			HRSRC hr = FindResource(hm, name, "RT_RCDATA");
-			size = SizeofResource(hm, hr);
-			buffer = static_cast<char*>(LockResource(LoadResource(hm, hr)));
-			if(hm==NULL || hr==NULL || size==0 || buffer==NULL){
-				return false;
-			}
-		}
-		return loadBuffer(buffer, size, "Resource");
+	return loadBuffer(buffer, size, "Resource");
+}
+bool scriptEngine::loadBuffer(const char *buffer, int size, const char *name){
+	if(buffer == NULL){
+		return false;
 	}
-	bool scriptEngine::loadBuffer(const char *buffer, int size, const char *name){
-		if(buffer == NULL){
-			return false;
-		}
-		if(!loadable){
-			yield();
-		}
-		bool ret = execBuffer(buffer, size, name);
-		return ret;
+	if(!loadable){
+		yield();
 	}
-	bool scriptEngine::loadBuffer(const char *buffer, int size){
-		return loadBuffer(buffer, size, SCRIPT_NAME_EMPTY);
+	bool ret = execBuffer(buffer, size, name);
+	return ret;
+}
+bool scriptEngine::loadBuffer(const char *buffer, int size){
+	return loadBuffer(buffer, size, SCRIPT_NAME_EMPTY);
+}
+bool scriptEngine::loadBuffer(const char *buffer, const char *name){
+	return loadBuffer(buffer, strlen(buffer),name);
+}
+bool scriptEngine::loadBuffer(const char *buffer){
+	return loadBuffer(buffer, strlen(buffer));
+}
+bool scriptEngine::reload(){
+	if(scriptName[0] == '\0'){
+		return false;
 	}
-	bool scriptEngine::loadBuffer(const char *buffer, const char *name){
-		return loadBuffer(buffer, strlen(buffer),name);
+	char fileName[256];
+	strncpy(fileName, scriptName, sizeof(fileName));
+	end();
+	init();
+	bool ret = loadFile(fileName);
+	if(ret){
+		start();
 	}
-	bool scriptEngine::loadBuffer(const char *buffer){
-		return loadBuffer(buffer, strlen(buffer));
-	}
-	bool scriptEngine::reload(){
-		if(scriptName[0] == '\0'){
-			return false;
-		}
-		char fileName[256];
-		strncpy(fileName, scriptName, sizeof(fileName));
-		end();
-		init();
-		bool ret = loadFile(fileName);
-		if(ret){
-			start();
-		}
-		return ret;
-	}
+	return ret;
+}
 
-	void scriptEngine::init(){
-		th105_active();
-		if(! initScript()){
-			printf("ERROR:scriptEngine init error(initScript return is false)\n");
-			getchar();
-			exit(0);
-		}
+void scriptEngine::init(){
+	th105_active();
+	if(! initScript()){
+		printf("ERROR:scriptEngine init error(initScript return is false)\n");
+		getchar();
+		exit(0);
 	}
-	void scriptEngine::start(){
-		if(! loadable){
-			printf("ERROR:scriptEngine start error(non load script)\n");
-			getchar();
-			exit(0);
-		}
-		th105_active();
-		startScript();
+}
+void scriptEngine::start(){
+	if(! loadable){
+		printf("ERROR:scriptEngine start error(non load script)\n");
+		getchar();
+		exit(0);
 	}
-	void scriptEngine::end(){
-		if(! loadable){
-			return;
-		}
-		endScript();
-		loadable = false;
-		scriptName[0] = '\0';
+	th105_active();
+	startScript();
+}
+void scriptEngine::end(){
+	if(! loadable){
+		return;
 	}
+	endScript();
+	loadable = false;
+	scriptName[0] = '\0';
+}
