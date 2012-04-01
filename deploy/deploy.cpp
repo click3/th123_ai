7630bad57d47a99707add4c52b2fc3b741505013
sweetie <bnryxx332a@mail.goo.ne.jp>
2012-04-01 20:45:30 +0900

    deployのインデントをスペースに

--- deploy/deploy.cpp
+++ deploy/deploy.cpp
@@ -24,109 +24,109 @@ namespace {
 typedef boost::shared_ptr<boost::remove_pointer<HKEY>::type> SHARED_REG_KEY;
 
 void MyRegCloseKey(HKEY key) {
-	const LSTATUS result = ::RegCloseKey(key);
-	BOOST_ASSERT(result == ERROR_SUCCESS);
+  const LSTATUS result = ::RegCloseKey(key);
+  BOOST_ASSERT(result == ERROR_SUCCESS);
 }
 
 SHARED_REG_KEY MyReadRegOpen(HKEY key, const wchar_t *sub_key) {
-	HKEY reg_key;
-	const LSTATUS result = ::RegOpenKeyExW(key, sub_key, 0, KEY_READ, &reg_key);
-	BOOST_ASSERT(result == ERROR_SUCCESS);
-	return SHARED_REG_KEY(reg_key, MyRegCloseKey);
+  HKEY reg_key;
+  const LSTATUS result = ::RegOpenKeyExW(key, sub_key, 0, KEY_READ, &reg_key);
+  BOOST_ASSERT(result == ERROR_SUCCESS);
+  return SHARED_REG_KEY(reg_key, MyRegCloseKey);
 }
 
 }
 
 bool GetRegString(std::wstring &out, HKEY key, const wchar_t *sub_key, const wchar_t *value_name) {
-	const SHARED_REG_KEY reg = MyReadRegOpen(key, sub_key);
-	DWORD type;
-	DWORD buffer_size;
-	const LSTATUS get_size_result = ::RegQueryValueExW(reg.get(), value_name, NULL, &type, NULL, &buffer_size);
-	if(get_size_result != ERROR_SUCCESS || type != REG_SZ || buffer_size == 0) {
-		return false;
-	}
-	out.resize(buffer_size / sizeof(wchar_t));
-	const LSTATUS result = ::RegQueryValueExW(reg.get(), value_name, NULL, NULL, reinterpret_cast<BYTE *>(&out.front()), &buffer_size);
-	if(result != ERROR_SUCCESS) {
-		return false;
-	}
-	out.resize(::wcsnlen(&out.front(), out.size()));
-	return true;
+  const SHARED_REG_KEY reg = MyReadRegOpen(key, sub_key);
+  DWORD type;
+  DWORD buffer_size;
+  const LSTATUS get_size_result = ::RegQueryValueExW(reg.get(), value_name, NULL, &type, NULL, &buffer_size);
+  if(get_size_result != ERROR_SUCCESS || type != REG_SZ || buffer_size == 0) {
+    return false;
+  }
+  out.resize(buffer_size / sizeof(wchar_t));
+  const LSTATUS result = ::RegQueryValueExW(reg.get(), value_name, NULL, NULL, reinterpret_cast<BYTE *>(&out.front()), &buffer_size);
+  if(result != ERROR_SUCCESS) {
+    return false;
+  }
+  out.resize(::wcsnlen(&out.front(), out.size()));
+  return true;
 }
 
 std::string history_path;
 
 const char * const list[] = {
-	"th123_ai.exe",	"SWRSAddr.ini",	"SwrAddr.ini",	"th123_ai.ini",
-	"../History.txt",	"../motion.txt",	"../README",	"../skill.txt",
-	"../spell.txt",	"../document.txt",	"../card.txt"
+  "th123_ai.exe",   "SWRSAddr.ini",     "SwrAddr.ini",  "th123_ai.ini",
+  "../History.txt", "../motion.txt",    "../README",    "../skill.txt",
+  "../spell.txt",   "../document.txt",  "../card.txt"
 };
 
 bool ParseArgs(unsigned int argc, const char * const *argv) {
-	boost::program_options::options_description opt("オプション");
-	opt.add_options()
-		("help,h",											"ヘルプを表示")
-		("version,v",	boost::program_options::value<std::string>(&history_path),		"versionを取得するhistory.txtのパス");
-	boost::program_options::variables_map vm;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, opt), vm);
-	boost::program_options::notify(vm);
-	if(vm.count("help") > 0 || vm.count("version") == 0) {
-		std::cout << opt << std::endl;
-		return false;
-	}
-	return true;
+  boost::program_options::options_description opt("オプション");
+  opt.add_options()
+    ("help,h",                                                                "ヘルプを表示")
+    ("version,v", boost::program_options::value<std::string>(&history_path),  "versionを取得するhistory.txtのパス");
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, opt), vm);
+  boost::program_options::notify(vm);
+  if(vm.count("help") > 0 || vm.count("version") == 0) {
+    std::cout << opt << std::endl;
+    return false;
+  }
+  return true;
 }
 
 int main(unsigned int argc, const char * const *argv) {
-	if(!::ParseArgs(argc, argv)) {
-		return 1;
-	}
+  if(!::ParseArgs(argc, argv)) {
+    return 1;
+  }
 
-	org::click3::Utility::SetAppDir();
+  org::click3::Utility::SetAppDir();
 
-	std::string app_dir_str;
-	if(!org::click3::Utility::GetAppDir(app_dir_str)) {
-		return 1;
-	}
-	const boost::filesystem::path app_dir = boost::filesystem::path(app_dir_str);
-	
-	std::string version;
-	if(!common::GetVersion(version, history_path.c_str())) {
-		return 1;
-	}
+  std::string app_dir_str;
+  if(!org::click3::Utility::GetAppDir(app_dir_str)) {
+    return 1;
+  }
+  const boost::filesystem::path app_dir = boost::filesystem::path(app_dir_str);
 
-	const boost::filesystem::path out_path = boost::filesystem::system_complete(app_dir / "../" / (version + ".zip"));
+  std::string version;
+  if(!common::GetVersion(version, history_path.c_str())) {
+    return 1;
+  }
 
-	std::wstring zip_app;
-	if(!::GetRegString(zip_app, HKEY_CURRENT_USER, L"Software\\HoeHoe\\Lhaplus", L"InstallPath")) {
-		return 1;
-	}
-	const boost::filesystem::path zip_app_path = boost::filesystem::path(zip_app) / "Lhaplus.exe";
+  const boost::filesystem::path out_path = boost::filesystem::system_complete(app_dir / "../" / (version + ".zip"));
 
-	std::wstring cmd = std::wstring(L"\"") + zip_app_path.wstring() + L"\" /c:zip /o:./";
+  std::wstring zip_app;
+  if(!::GetRegString(zip_app, HKEY_CURRENT_USER, L"Software\\HoeHoe\\Lhaplus", L"InstallPath")) {
+    return 1;
+  }
+  const boost::filesystem::path zip_app_path = boost::filesystem::path(zip_app) / "Lhaplus.exe";
 
-	BOOST_FOREACH(const char * const p, list) {
-		const boost::filesystem::path filename = boost::filesystem::system_complete(app_dir / p);
-		wchar_t add_cmd[256];
-		WSPRINTF(add_cmd, L" \"%s\"", filename.wstring().c_str());
-		cmd.append(add_cmd);
-	}
+  std::wstring cmd = std::wstring(L"\"") + zip_app_path.wstring() + L"\" /c:zip /o:./";
 
-	STARTUPINFOW si;
-	PROCESS_INFORMATION pi;
-	::ZeroMemory(&si, sizeof(si));
-	::ZeroMemory(&pi, sizeof(pi));
-	si.cb = sizeof(si);
-	cmd.resize(cmd.size()+1);
-	cmd[cmd.size()-1] = '\0';
-	if(0 == ::CreateProcessW(zip_app_path.c_str(), &cmd.front(), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
-		return 1;
-	}
-	if(WAIT_OBJECT_0 != ::WaitForSingleObject(pi.hProcess, INFINITE)) {
-		return 1;
-	}
-	::CloseHandle(pi.hThread);
-	::CloseHandle(pi.hProcess);
-	::system((std::string("move th123_ai.zip ") + out_path.string()).c_str());
-	return 0;
+  BOOST_FOREACH(const char * const p, list) {
+    const boost::filesystem::path filename = boost::filesystem::system_complete(app_dir / p);
+    wchar_t add_cmd[256];
+    WSPRINTF(add_cmd, L" \"%s\"", filename.wstring().c_str());
+    cmd.append(add_cmd);
+  }
+
+  STARTUPINFOW si;
+  PROCESS_INFORMATION pi;
+  ::ZeroMemory(&si, sizeof(si));
+  ::ZeroMemory(&pi, sizeof(pi));
+  si.cb = sizeof(si);
+  cmd.resize(cmd.size()+1);
+  cmd[cmd.size()-1] = '\0';
+  if(0 == ::CreateProcessW(zip_app_path.c_str(), &cmd.front(), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+    return 1;
+  }
+  if(WAIT_OBJECT_0 != ::WaitForSingleObject(pi.hProcess, INFINITE)) {
+    return 1;
+  }
+  ::CloseHandle(pi.hThread);
+  ::CloseHandle(pi.hProcess);
+  ::system((std::string("move th123_ai.zip ") + out_path.string()).c_str());
+  return 0;
 }
